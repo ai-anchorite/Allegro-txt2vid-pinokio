2124ad7c0252ca2cc08390db72d482c10c4185cb
ai_anchorite <ai_anchorite@proton.me>
2024-10-26 02:02:25 +1300

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -73,7 +73,7 @@ module.exports = {
           icon: "fa-regular fa-circle-xmark",
           text: "Reset",
           href: "reset.js",
-		  confirm: "Are you sure you wish to reset this app?"
+	  confirm: "Are you sure you wish to reset this app?"
         }]
       }
     } else {
